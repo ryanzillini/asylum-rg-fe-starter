dd4d70ce482c800f6a84a35e5842823a7f2dabad
ryanzillini <rzillini@gameblazers.com>
2024-01-09 16:01:52 -0500

    first-ticket-complete

--- src/components/pages/Landing/RenderLandingPage.jsx
+++ src/components/pages/Landing/RenderLandingPage.jsx
@@ -98,7 +98,43 @@ function RenderLandingPage(props) {
       </div>
       <div>
         {/* Bottom Section: Add code here for the graphs section for your first ticket */}
-        {/* <div className="bottom-section">*/}
+        <div className="bottom-section">
+          <h2 className="stat-header">Systemic Disparity Insights</h2>
+          <div className="statistic-container">
+            <div className="stat">
+              <h2 className="stat-number">36%</h2>
+              <p>
+                By the end of the Trump administration, the average asylum
+                office grant rate had fallen 36 percent from an average of 44
+                percent in fiscal year 2016 to 28 percent in fiscal year 2020
+              </p>
+            </div>
+            <div className="stat">
+              <h2 className="stat-number">5%</h2>
+              <p>
+                The New York asylum office grant rate dropped to 5 perecnt in
+                fiscal year 2020
+              </p>
+            </div>
+            <div className="stat">
+              <h2 className="stat-number">6x Lower</h2>
+              <p>
+                Between fiscal year 2017 and 2020, the New York asylum office's
+                average grant rate was six times lower than the San Franciso
+                asylum office
+              </p>
+            </div>
+          </div>
+          <div className="read-more-btn-container">
+            <Button
+              type="default"
+              style={{ backgroundColor: '#404C4A', color: '#FFFFFF' }}
+              className="read-more-btn-1"
+            >
+              Read More
+            </Button>
+          </div>
+        </div>
         <p onClick={() => scrollToTop()} className="back-to-top">
           Back To Top ^
         </p>
