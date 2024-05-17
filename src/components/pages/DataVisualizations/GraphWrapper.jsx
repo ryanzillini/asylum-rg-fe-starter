20e9356f3aa0778a33c33cbef76138b0e1ebc18f
ryanzillini <ryanzillini@gmail.com>
2024-05-17 14:13:11 -0400

    secondTicket completed

--- src/components/pages/DataVisualizations/GraphWrapper.jsx
+++ src/components/pages/DataVisualizations/GraphWrapper.jsx
@@ -10,11 +10,12 @@ import YearLimitsSelect from './YearLimitsSelect';
 import ViewSelect from './ViewSelect';
 import axios from 'axios';
 import { resetVisualizationQuery } from '../../../state/actionCreators';
-import test_data from '../../../data/test_data.json';
 import { colors } from '../../../styles/data_vis_colors';
 import ScrollToTopOnMount from '../../../utils/scrollToTopOnMount';
 
 const { background_color } = colors;
+
+const BASE_URL = 'https://hrf-asylum-be-b.herokuapp.com/cases';
 
 function GraphWrapper(props) {
   const { set_view, dispatch } = props;
@@ -50,65 +51,60 @@ function GraphWrapper(props) {
         break;
     }
   }
-  function updateStateWithNewData(years, view, office, stateSettingCallback) {
-    /*
-          _                                                                             _
-        |                                                                                 |
-        |   Example request for once the `/summary` endpoint is up and running:           |
-        |                                                                                 |
-        |     `${url}/summary?to=2022&from=2015&office=ZLA`                               |
-        |                                                                                 |
-        |     so in axios we will say:                                                    |
-        |                                                                                 |     
-        |       axios.get(`${url}/summary`, {                                             |
-        |         params: {                                                               |
-        |           from: <year_start>,                                                   |
-        |           to: <year_end>,                                                       |
-        |           office: <office>,       [ <-- this one is optional! when    ]         |
-        |         },                        [ querying by `all offices` there's ]         |
-        |       })                          [ no `office` param in the query    ]         |
-        |                                                                                 |
-          _                                                                             _
-                                   -- Mack 
-    
-    */
-
-    if (office === 'all' || !office) {
+  async function updateStateWithNewData(
+    years,
+    view,
+    office,
+    stateSettingCallback
+  ) {
+    if (view === 'citizenship') {
       axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-          },
-        })
+        .get(`${BASE_URL}/citizenshipSummary`)
         .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
+          const citizenshipResults = [{ citizenshipResults: result.data }];
+          stateSettingCallback(view, office, citizenshipResults); //! put the result.data in array in order for plotly to receive data
         })
         .catch(err => {
           console.error(err);
         });
     } else {
-      axios
-        .get(process.env.REACT_APP_API_URI, {
-          // mock URL, can be simply replaced by `${Real_Production_URL}/summary` in prod!
-          params: {
-            from: years[0],
-            to: years[1],
-            office: office,
-          },
-        })
-        .then(result => {
-          stateSettingCallback(view, office, test_data); // <-- `test_data` here can be simply replaced by `result.data` in prod!
-        })
-        .catch(err => {
-          console.error(err);
-        });
+      if (office === 'all' || !office) {
+        axios
+          .get(`${BASE_URL}/fiscalSummary`, {
+            params: {
+              from: years[0],
+              to: years[1],
+            },
+          })
+          .then(result => {
+            stateSettingCallback(view, office, [result.data]); //! put the result.data in array in order for plotly to receive data
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      } else {
+        axios
+          .get(`${BASE_URL}/fiscalSummary`, {
+            params: {
+              from: years[0],
+              to: years[1],
+              office: office,
+            },
+          })
+          .then(result => {
+            stateSettingCallback(view, office, [result.data]); //! put the result.data in array in order for plotly to receive data
+          })
+          .catch(err => {
+            console.error(err);
+          });
+      }
     }
   }
+
   const clearQuery = (view, office) => {
     dispatch(resetVisualizationQuery(view, office));
   };
+
   return (
     <div
       className="map-wrapper-container"
